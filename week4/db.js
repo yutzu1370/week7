e7cb4a15e648075f4279f341b9e775e11cecaa40
yutzu1370 <doim1370523@gmail.com>
2025-02-18 16:04:41 +0800

    week4完成

--- week4/db.js
+++ week4/db.js
@@ -10,7 +10,52 @@ const CreditPackage = new EntitySchema({
       generated: "uuid",
       nullable: false,
     },
-  },
+    name: {
+      type: "varchar",
+      length: 50,
+      nullable: false,
+      unique: true
+    },
+    credit_amount: {
+      type: "integer",
+      nullable: false
+    },
+    price: {
+      type: "numeric",
+      precision: 10, // 總共 10 位數
+      scale: 2, //小數點 2 位
+      nullable: false
+    },
+    created_at: {
+      type: "timestamp",
+      createDate: true, // 自動產生時間
+      nullable: false
+    }
+  }
+})
+
+const Skill = new EntitySchema({
+  name: "Skill",
+  tableName: "SKILL",
+  columns: {
+    id: {
+      primary: true,
+      type: "uuid",
+      generated: "uuid",
+      nullable: false
+    },
+    name: {
+      type: "varchar",
+      length: 50,
+      nullable: false,
+      unique: true
+    },
+    created_at: {
+      type: "timestamp",
+      createDate: true, // 自動產生時間
+      nullable: false
+    }
+  }
 })
 
 const AppDataSource = new DataSource({
@@ -20,8 +65,14 @@ const AppDataSource = new DataSource({
   username: process.env.DB_USERNAME || "root",
   password: process.env.DB_PASSWORD || "test",
   database: process.env.DB_DATABASE || "test",
-  entities: [CreditPackage],
+  entities: [CreditPackage, Skill],
   synchronize: true,
 })
+
+// 透過 entities 陣列將所有 EntitySchema 加入。
+
+// 啟動時 TypeORM 會根據這些設定自動建立或更新表結構（若 synchronize: true）。
+
+// 之後就能使用 AppDataSource.getRepository("CreditPackage") 或 AppDataSource.getRepository("Skill") 進行 CRUD。
 
 module.exports = AppDataSource
